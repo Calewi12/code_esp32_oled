98b8521d0831854a410dc3ee1333aecaac15f3a2
Calewi12 <calewi12@asu.edu>
2025-03-02 19:58:52 -0700

    fun changes

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ state = 0
 
 while True:
     state += 1
-    if state >= 4:\
+    if state >= 4:
         state = 0
 
     print("Current State:", state)
 
     if state == 0:
-        my_oled.print_text("This is ground control to Major Tom", 0, 0)
+        text = "This is ground control to Major Tom"
+        for i in range(len(text) * -6, 128, 1):  # Scroll left to right
+            my_oled.oled.fill(0)  # Clear the screen
+            my_oled.print_text(text, i, 0)  # Move text dynamically
+            my_oled.oled.show()
+            time.sleep(0.05)  # Adjust scrolling speed
 
     elif state == 1:
         my_oled.print_text("I'm stepping through the door", 0, 48)  # Bottom row
@@ -27,3 +32,4 @@ while True:
         my_oled.oled.show()
 
     time.sleep(1)
+
