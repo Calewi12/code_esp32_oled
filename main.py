10a3e9e8fb2e8eaea8969e4c1e070cb4043ca099
Calewi12 <calewi12@asu.edu>
2025-03-02 21:07:08 -0700

    Final changes

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import time
 state = 0
 
 while True:
-    state += 1
-    if state >= 4:
-        state = 0
+   
 
     print("Current State:", state)
 
@@ -18,7 +16,7 @@ while True:
             my_oled.oled.fill(0)  # Clear the screen
             my_oled.print_text(text, i, 0)  # Move text dynamically
             my_oled.oled.show()
-            time.sleep(0.00001)  # Adjust scrolling speed
+            time.sleep(0.005)  # Adjust scrolling speed
 
     elif state == 1:
         text = "I'm stepping through the door"
@@ -26,19 +24,25 @@ while True:
     
         for i in range(128, -text_width, -1):  # Move from right to left
          my_oled.oled.fill(0)  # Clear the screen
-        my_oled.print_text(text, i, 48)  # Display moving text on bottom row
-        my_oled.oled.show()
-        time.sleep(0.05)  # Adjust scrolling speed
+         my_oled.print_text(text, i, 48)  # Display moving text on bottom row
+         my_oled.oled.show()
+         time.sleep(0.005)  # Adjust scrolling speed
 
     elif state == 2:
         my_oled.oled.fill(0)  # Clear the screen
         my_oled.oled.line(0, 63, 127, 0, 1)  # Bottom-left to top-right
         my_oled.oled.show()
+        time.sleep(1)
 
     elif state == 3:
         my_oled.oled.fill(0)  # Clear the screen
         my_oled.graphics.fill_rect(64, 32, 64, 32, 1)  # Bottom-right quadrant
         my_oled.oled.show()
-
         time.sleep(1)
+
+    state += 1
+    if state >= 4:
+        state = 0
+        
+        
 
