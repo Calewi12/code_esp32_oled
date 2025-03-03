19cf304a743bff78eccf2a84c1ce22fd07bce277
Calewi12 <calewi12@asu.edu>
2025-03-02 18:45:26 -0700

    implement and state machine

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import my_oled
 import time
+
+state = 0
+
 while True:
+    state += 1
+    if state >= 4:
+        state = 0
+
+    print("Current State:", state)
+
+    if state == 0:
+        my_oled.print_text("test", 0, 0)
+
+    elif state == 1:
+        my_oled.print_text("something else", 0, 48)  # Bottom row
+
+    elif state == 2:
+        my_oled.oled.fill(0)  # Clear the screen
+        my_oled.oled.line(0, 63, 127, 0, 1)  # Bottom-left to top-right
+        my_oled.oled.show()
+
+    elif state == 3:
+        my_oled.oled.fill(0)  # Clear the screen
+        my_oled.graphics.fill_rect(64, 32, 64, 32, 1)  # Bottom-right quadrant
+        my_oled.oled.show()
+
     time.sleep(1)
